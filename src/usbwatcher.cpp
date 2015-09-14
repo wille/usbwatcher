50f802d1de6d1b22ba815b2ccf79e01fa297b252
redpois0n <red@cmail.nu>
2015-09-14 21:54:26 +0200

    Iterate windows drives, skip fixed etc

--- src/usbwatcher.cpp
+++ src/usbwatcher.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 
+#ifdef _WIN32
+#	include <windows.h>
+#endif
+
 using namespace std;
 
-int main(int argc, char* argv[]) {
+void iterate();
 
+int main(int argc, char* argv[]) {
+	iterate();
+}
+
+void iterate() {
+#ifdef _WIN32
+	const int buflen = 256;
+	char buffer[buflen];
+
+	char *temp = buffer;
+
+	DWORD result = GetLogicalDriveStrings(buflen, buffer);
+
+	if (result == 0) {
+		cerr << "Failed to get drives" << endl;
+		exit(0);
+	}
+
+	while (*temp) {
+		int type = GetDriveType(temp);
+
+		if (type == DRIVE_UNKNOWN) {
+			printf("%s : Unknown Drive type\n", temp);
+		} else if (type == DRIVE_REMOVABLE) {
+			printf("%s : Removable Drive\n", temp);
+		} else if (type == DRIVE_CDROM) {
+			printf("%s : CD-Rom/DVD-Rom\n", temp);
+		} else if (type == DRIVE_RAMDISK) {
+			printf("%s : Ram Drive\n", temp);
+		} else {
+			cout << "Skipping drive " << temp << endl;
+		}
+
+		temp += lstrlen(temp) + 1;
+	}
+
+#endif
 }
