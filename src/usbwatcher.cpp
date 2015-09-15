fd1bf20775670cc89b842317846fd5d6bb3bdbf1
redpois0n <red@cmail.nu>
2015-09-15 10:15:26 +0200

    loop linux mounted drives, iterates and sleeps

--- src/usbwatcher.cpp
+++ src/usbwatcher.cpp
@@ -2,14 +2,34 @@
 
 #ifdef _WIN32
 #	include <windows.h>
+#elif defined(__linux__)
+#   include <fstream>
+#	include <unistd.h>
 #endif
+
+#define DELAY 1000
 
 using namespace std;
 
+struct mount {
+	string device;
+	string destination;
+	string fstype;
+	string options;
+	int dump;
+	int pass;
+};
+
 void iterate();
+void sleep();
 
 int main(int argc, char* argv[]) {
-	iterate();
+
+	for (;;) {
+		sleep();
+		iterate();
+	}
+
 }
 
 void iterate() {
@@ -44,5 +64,23 @@ void iterate() {
 		temp += lstrlen(temp) + 1;
 	}
 
+#elif defined(__linux__)
+	ifstream mountInfo("/proc/mounts");
+
+	while (!mountInfo.eof()) {
+		mount mount;
+		mountInfo >> mount.device >> mount.destination >> mount.fstype >> mount.options >> mount.dump >> mount.pass;
+		if (!mount.device.empty()) {
+			cout << mount.fstype << " device \"" << mount.device << "\", mounted on \"" << mount.destination << "\". Options: " << mount.options << ". Dump:" << mount.dump << " Pass:" << mount.pass << endl;
+		}
+	}
+#endif
+}
+
+void sleep() {
+#ifdef _WIN32
+	Sleep(DELAY);
+#else
+	usleep(DELAY * 1000);
 #endif
 }
